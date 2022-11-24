d38849e566da5d0b236d4a0e3838b72f96ae484e
Joaquin Pareja <joaq.pareja@gmail.com>
2022-11-23 23:49:41 -0300

    last touches

--- cypress/support/POM/Products.js
+++ cypress/support/POM/Products.js
@@ -7,81 +7,43 @@ let pTxtTitleSecondProduct;
 let test = []; //Create an empty array to then be able to add new items
 const filename = '/EnviromentVariables.json';
  class StoreProductInformation{
-
-        getFirstProduct(){
-    const firstProduct =
-                cy.get(productsPage.divFirstProduct)
-                    .find(productsPage.h2ProductTitle)
-                        .then(response => {
-                            h2TxtPriceFirstProduct = response.text();
-                                cy.log(h2TxtPriceFirstProduct);  
-                                test.push({'priceFirstProduct': h2TxtPriceFirstProduct})
-                                cy.writeFile(filename,  test);
-                                    cy.readFile(filename).then((str)=>{
-                                        cy.log(str);
-                                         cy.log(str[0]);
-                                         const arr = str[0]
-                                         cy.log(arr.priceFirstProduct);
-                            });
-                        });
-                        cy.readFile(filename).then((str)=>{
-                            cy.log(str);
-                            const arr = str[0]
-                            cy.log(arr.priceFirstProduct);
-                        });
- 
-                cy.get(productsPage.divFirstProduct)
-                     .find(productsPage.pProductTitle)
+    getFirstProduct(){
+        const firstProduct =
+            cy.get(productsPage.divFirstProduct)
+                .find(productsPage.h2ProductTitle)
+                    .then(response => {
+                        h2TxtPriceFirstProduct = response.text();
+                            test.push({'priceFirstProduct': h2TxtPriceFirstProduct})
+                                cy.writeFile(filename,  test);       
+                    });
+            cy.get(productsPage.divFirstProduct)
+                    .find(productsPage.pProductTitle)
                         .then(response => {
                             pTxtTitleFirstProduct = response.text();
-                            cy.log(pTxtTitleFirstProduct);
-                            test.push({'titleFirstProduct': pTxtTitleFirstProduct})
-                                cy.writeFile(filename,  test);
-                                    cy.readFile(filename).then((str)=>{
-                                        cy.log(str);
-                                         const arr = str
-                                         cy.log(arr[1].titleFirstProduct);
-                             });   
+                                test.push({'titleFirstProduct': pTxtTitleFirstProduct})
+                                    cy.writeFile(filename,  test);
                             });                                  
-          return firstProduct;
-    }
+        return firstProduct;
+        }
     getSecondProduct(){
-
         const secondProduct =
             cy.get(productsPage.divSecondProduct)
                 .find(productsPage.h2ProductTitle)
                     .then(response => {
                         h2TxtPriceSecondProduct = response.text();
-                        cy.log(h2TxtPriceSecondProduct);
-                        test.push({'priceSecondProduct': h2TxtPriceSecondProduct})
-                        cy.writeFile(filename, test);
-                        cy.readFile(filename).then((str)=>{
-                            cy.log(str);
-                             const arr = str
-                             cy.log(arr[2].priceSecondProduct);
-                 });   
+                                test.push({'priceSecondProduct': h2TxtPriceSecondProduct})
+                                    cy.writeFile(filename, test);   
                 });  
             cy.get(productsPage.divSecondProduct)
                 .find(productsPage.pProductTitle)
                     .then(response => {
                         pTxtTitleSecondProduct = response.text();
-                        test.push({'titleSecondProduct': pTxtTitleSecondProduct})
-                        cy.writeFile(filename,  test);
-                        cy.readFile(filename).then((str)=>{
-                            cy.log(str);
-                             const arr = str
-                            //  let titleFirstProduct = arr[1].titleFirstProduct;
-                            //  let titleSecondProduct = arr[3].titleSecondProduct
-                            // //  expect(titleFirstProduct).to.not.equal(titleSecondProduct).pause(); //An assertion within the assertion
-                             cy.log(arr[1].titleFirstProduct);
-                             cy.log(arr[3].titleSecondProduct);
-                 });   
-  
+                            test.push({'titleSecondProduct': pTxtTitleSecondProduct})
+                                cy.writeFile(filename, test);  
                     });                    
   return secondProduct
     } 
 }
-
  class AddProducts{
   //Will create an empty contructor
         getFirstProduct() {
@@ -104,7 +66,6 @@ const filename = '/EnviromentVariables.json';
                         .click(); 
             return secondProduct;                             
         }
- 
         getThirdProduct(){
             const thirdProduct =  
                 cy.get(productsPage.divThirdProductOnHover)   
@@ -115,7 +76,6 @@ const filename = '/EnviromentVariables.json';
                 .click();                     
             return thirdProduct;          
         }
-
         getLastProduct(){
             const lastProduct =  
                 cy.get(productsPage.divLastProductOnHover)
@@ -127,7 +87,6 @@ const filename = '/EnviromentVariables.json';
         return lastProduct;   
         }
 }
-
 class AddRecommendedProducts {
     
     //if the class active then do the following method else do the second one
